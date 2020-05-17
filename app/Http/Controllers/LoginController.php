ce200d79f05c9bc8317d76bdad2ad375eaea259e
Rein Terry <58175822+Dragorein@users.noreply.github.com>
2020-05-17 18:52:57 +0700

    jgn ada yg merge
    dah pasrah gw anj sama login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,20 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\user;
+
 
 class LoginController extends Controller
 {
-    use AuthenticatesUsers;
+    public function init(){
+        $user = Auth::user();
 
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+        return response()->json(['user' => $user], 200);
     }
 
-    public function index(){
-        return view('landing');
+    public function login(Request $request){
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password], true)){ //true remember password
+            return response()->json(Auth::user(), 200,);
+        } else{
+            return response()->json(['error' => 'Maaf anda tidak bisa login.'], 401);
+        }
     }
 
+    public function logout(){
+        Auth::logout();
+    }
 }
